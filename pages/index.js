641e5d13fc4c961f7bf64a5b2d9aeaa9ddd50f04
Anna Rolfes <anna-rolfes@gmx.de>
2022-05-25 11:08:08 +0200

    Bugfixing for vercel deployment

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import ListEntry from '../components/ListEntry';
-import RecipeForm from '../components/recipeForm';
-
 export default function Home() {
   return (
     <>
-      <ListEntry>Strawberry Cake</ListEntry>
-      <ListEntry>Chocolate Muffins</ListEntry>
-      <ListEntry>Coffee Cake</ListEntry>
-      <RecipeForm></RecipeForm>
+      <h1>Hallo</h1>
     </>
   );
 }
