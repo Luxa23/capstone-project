7f500857076de67a68bc67b782a9ed43768432b8
Anna Rolfes <anna-rolfes@gmx.de>
2022-05-31 11:42:56 +0200

    Changed localstore code

--- components/RecipeForm.js
+++ components/RecipeForm.js
@@ -5,39 +5,44 @@ import {
 } from '../components/StyledComponents';
 import { useState } from 'react';
 import useStore from '../hooks/useStore';
+import useHydration from '../hooks/useHydration';
 
 export default function RecipeForm() {
   const [inputValue, setInputValue] = useState('');
 
   const addRecipe = useStore(state => state.addRecipe);
 
+  const hydrated = useHydration();
+
   return (
     <>
-      <StyledForm
-        onSubmit={event => {
-          event.preventDefault();
-          const trimmedValue = inputValue.trim();
-          if (trimmedValue.length > 0) {
-            addRecipe(trimmedValue);
-            setInputValue('');
-          }
-        }}
-      >
-        <StyledInput
-          required
-          type="text"
-          size="30"
-          minLength={2}
-          maxLength={100}
-          placeholder="Add recipe titles"
-          value={inputValue}
-          onChange={event => {
-            setInputValue(event.target.value);
+      {hydrated && (
+        <StyledForm
+          onSubmit={event => {
+            event.preventDefault();
+            const trimmedValue = inputValue.trim();
+            if (trimmedValue.length > 0) {
+              addRecipe(trimmedValue);
+              setInputValue('');
+            }
           }}
-        />
+        >
+          <StyledInput
+            required
+            type="text"
+            size="30"
+            minLength={2}
+            maxLength={100}
+            placeholder="Add recipe titles"
+            value={inputValue}
+            onChange={event => {
+              setInputValue(event.target.value);
+            }}
+          />
 
-        <StyledButton type="submit">Save</StyledButton>
-      </StyledForm>
+          <StyledButton type="submit">Save</StyledButton>
+        </StyledForm>
+      )}
     </>
   );
 }
