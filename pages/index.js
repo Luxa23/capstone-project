af578ae9d8ac42178139fae3f4e1db3a1aca5346
Anna Rolfes <anna-rolfes@gmx.de>
2022-06-13 12:26:04 +0200

    Added logic to home page

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,28 @@ import PlusIcon from '../public/plusicon.svg';
 
 export default function Home() {
   const recipeList = useStore(state => state.recipeList);
+
   return (
     <>
       <StyledH1Home>my recipes</StyledH1Home>
-      <StyledTextHome>
-        Welcome to my app &quot;my recipes&quot;. Add your first recipe with the
-        plus-icon.
-      </StyledTextHome>
-      <StyledUl>
-        {recipeList.map(recipe => {
-          return (
-            <ListEntry id={recipe.id} key={recipe.id} recipe={recipe}>
-              {recipe.recipeTitle}
-            </ListEntry>
-          );
-        })}
-      </StyledUl>
+      {recipeList.length === 0 && (
+        <StyledTextHome>
+          Welcome to my app &quot;my recipes&quot;. Add your first recipe with
+          the plus-icon.
+        </StyledTextHome>
+      )}
+      {recipeList.length > 0 && (
+        <StyledUl>
+          {recipeList.map(recipe => {
+            return (
+              <ListEntry id={recipe.id} key={recipe.id} recipe={recipe}>
+                {recipe.recipeTitle}
+              </ListEntry>
+            );
+          })}
+        </StyledUl>
+      )}
+
       <Link passHref href="/new-recipe">
         <StyledButton className="button--plusicon">
           <PlusIcon width="75px" height="75px" />
