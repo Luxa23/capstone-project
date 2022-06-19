d6706505ca9ab9d85dd6ed27345267ddca8eefe7
Anna Rolfes <anna-rolfes@gmx.de>
2022-06-19 22:52:41 +0200

    Styled radio buttons nicely

--- components/StyledComponents/StyledRadioButtonGroup.js
+++ components/StyledComponents/StyledRadioButtonGroup.js
@@ -5,10 +5,41 @@ const StyledRadioButtonGroup = styled.fieldset`
   font-size: 16;
   color: #666666;
   border: none;
-  margin: 0 0 16px;
+
+  label {
+    height: 1.5rem;
+    padding: 0.25rem;
+  }
 
   input {
-    width: 3rem;
+    appearance: none;
+    position: relative;
+    top: 0.4rem;
+    bottom: 0;
+    left: 0;
+    height: 1.5rem;
+    width: 1.5rem;
+    transition: all 0.15s ease-out 0s;
+    background: pink;
+    _border: none;
+    color: green;
+    _display: inline-block;
+    margin-right: 0.75rem;
+    _outline: none;
+    _position: relative;
+    _z-index: 1000;
+    border-radius: 50%;
+  }
+
+  input:checked {
+    background: yellow;
+  }
+
+  input:checked::before {
+    height: 1.5rem;
+    width: 1.5rem;
+    display: flex;
+    _content: '\f00c';
   }
 
   &.radiobuttons-nextToEachOther {
