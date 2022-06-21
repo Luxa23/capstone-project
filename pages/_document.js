cb6bc73b0e5c071a890bdcdbbdaea5b3de76dec4
Anna Rolfes <anna-rolfes@gmx.de>
2022-06-21 11:31:53 +0200

    Fixed crossorigin bug

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,11 @@ export default class MyDocument extends Document {
       <Html lang="de">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600;700&display=swap"
             rel="stylesheet"
