42392fd9717452d70624592fb7741936586a6fa0
Anna Rolfes <anna-rolfes@gmx.de>
2022-06-25 22:03:07 +0200

    Positioned radio buttons on home correctly

--- components/StyledComponents/StyledTextHome.js
+++ components/StyledComponents/StyledTextHome.js
@@ -6,8 +6,7 @@ const StyledTextHome = styled.p`
   margin: 0;
   padding: 0 2rem 0 3.5rem;
   align-self: start;
-  grid-row-start: 3;
-  grid-row-end: 4;
+  grid-row-start: 4;
   grid-column-start: 1;
   grid-column-end: 2;
 `;
